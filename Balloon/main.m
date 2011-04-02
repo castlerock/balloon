ed8f31d530bfdf9d48ae7ce448a00efca9a38ba9
Mark Madsen <mark@agileanimal.com>
2011-04-01 23:11:41 -0600

    Initial Commit

---
+++ Balloon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Balloon
+//
+//  Created by Mark Madsen on 4/1/11.
+//  Copyright 2011 AGiLE ANiMAL INC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
